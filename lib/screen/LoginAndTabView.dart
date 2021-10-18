effbe61bba17d2e079945f75f5d1d9bd9fed7f57
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2021-10-18 23:47:57 +0530

    TabView.dart -> LoginAndTabView.dart

---
+++ lib/screen/LoginAndTabView.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:movieapp/screen/ActivityFeed.dart';
+import 'package:movieapp/screen/Profile.dart';
+import 'package:movieapp/screen/Upload.dart';
+
+final GoogleSignIn googleSignIn = GoogleSignIn();
+
+class TabView extends StatefulWidget {
+  TabView();
+
+  @override
+  _TabViewState createState() => _TabViewState();
+}
+
+class _TabViewState extends State<TabView> {
+  bool isAuth = false;
+  late PageController pageController;
+  int pageIndex = 0;
+  bool loading = true;
+
+  handleSignIn(GoogleSignInAccount account) {
+    if (account != null) {
+      setState(() {
+        isAuth = true;
+      });
+    } else {
+      setState(() {
+        isAuth = false;
+      });
+    }
+    setState(() {
+      loading = false;
+    });
+    print(account.email);
+    print("isAuth: " + isAuth.toString());
+    print("loading: " + loading.toString());
+  }
+
+  login() {
+    googleSignIn.signIn();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+    googleSignIn.onCurrentUserChanged.listen((account) {
+      handleSignIn(account!);
+    }, onError: (err) {
+      print('Error signing in: $err');
+    });
+    googleSignIn.signInSilently(suppressErrors: true).then((account) {
+      handleSignIn(account!);
+    }).catchError((err) {
+      print('Error signing in: $err');
+    });
+    setState(() {
+      loading = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  onPageChanged(int pageIndex) {
+    setState(() {
+      this.pageIndex = pageIndex;
+    });
+  }
+
+  onTap(int pageIndex) {
+    pageController.animateToPage(
+      pageIndex,
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  Scaffold signedOutUser() {
+    return Scaffold(
+        body: Container(
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.all(15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(top: 60),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      print("Sign in");
+                      login();
+                    },
+                    child: Text(
+                      "Sign in",
+                      style: TextStyle(fontFamily: "Poppins"),
+                    ),
+                  ),
+                )
+              ],
+            )));
+  }
+
+  Scaffold signedInUser() {
+    return Scaffold(
+      body: PageView(
+        children: <Widget>[ActivityFeed(), Upload(), Profile()],
+        controller: pageController,
+        onPageChanged: onPageChanged,
+        physics: NeverScrollableScrollPhysics(),
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+          currentIndex: pageIndex,
+          onTap: onTap,
+          activeColor: Theme.of(context).primaryColor,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.explore), label: "Explore"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.upload,
+                  size: 35.0,
+                ),
+                label: "Upload"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.supervised_user_circle), label: "profile"),
+          ]),
+    );
+  }
+
+  Container loadingUserScreen() {
+    return Container(
+        color: Colors.white,
+        alignment: Alignment.center,
+        padding: EdgeInsets.only(top: 10.0),
+        child: CircularProgressIndicator());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (isAuth == true) {
+      return signedInUser();
+    } else {
+      return signedOutUser();
+    }
+  }
+}
