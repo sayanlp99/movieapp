02dbfcf4e0dada7b18d02c7b4aa0d1457da592d0
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2021-10-19 10:36:44 +0530

    Firebase Storage file upload

--- lib/screen/Upload.dart
+++ lib/screen/Upload.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:movieapp/screen/LoginAndTabView.dart';
+import 'package:path/path.dart';
 
 final movieRef = FirebaseFirestore.instance.collection('movie');
 
@@ -15,9 +20,13 @@ class _UploadState extends State<Upload> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _directorController = TextEditingController();
   final TextEditingController _picUrlController = TextEditingController();
+  UploadTask? task;
+  File? file;
+  late final String picUrl;
 
   @override
   Widget build(BuildContext context) {
+    final fileName = file != null ? basename(file!.path) : 'No File Selected';
     return Scaffold(
         body: SingleChildScrollView(
       child: SafeArea(
@@ -37,20 +46,23 @@ class _UploadState extends State<Upload> {
                 decoration: InputDecoration(
                     labelText: "Director Name", border: OutlineInputBorder()),
               )),
-          Container(
-              margin: EdgeInsets.all(20),
-              child: TextField(
-                controller: _picUrlController,
-                decoration: InputDecoration(
-                    labelText: "Pic url", border: OutlineInputBorder()),
-              )),
+          // Container(
+          //     margin: EdgeInsets.all(20),
+          //     child: TextField(
+          //       controller: _picUrlController,
+          //       decoration: InputDecoration(
+          //           labelText: "Pic url", border: OutlineInputBorder()),
+          //     )),
           ElevatedButton(
               onPressed: () {
-                submitData(
-                    _nameController.text,
-                    _directorController.text,
-                    _picUrlController.text,
-                    googleSignIn.currentUser!.email.toString());
+                selectFile();
+              },
+              child: Text("Add image")),
+          ElevatedButton(
+              onPressed: () async {
+                picUrl = await uploadFile();
+                submitData(_nameController.text, _directorController.text,
+                    picUrl, googleSignIn.currentUser!.email.toString());
               },
               child: Text("Add"))
         ],
@@ -58,10 +70,46 @@ class _UploadState extends State<Upload> {
     ));
   }
 
-  void submitData(String text, String text2, String text3, String user) async {
+  void submitData(String text, String text2, String pic, String user) async {
     movieRef
-        .add({"name": text, "director": text2, "picUrl": text3, "user": user});
-    Navigator.of(context)
-        .push(MaterialPageRoute(builder: (context) => TabView()));
+        .add({"name": text, "director": text2, "picUrl": pic, "user": user});
+    // Navigator.of(context)
+    //     .push(MaterialPageRoute(builder: (context) => TabView()));
+  }
+
+  Future selectFile() async {
+    final result = await FilePicker.platform.pickFiles(allowMultiple: false);
+
+    if (result == null) return;
+    final path = result.files.single.path!;
+
+    setState(() => file = File(path));
+  }
+
+  Future<String> uploadFile() async {
+    if (file == null) return "Empty";
+
+    final fileName = basename(file!.path);
+    final destination = 'movie_poster/$fileName';
+
+    task = uploadImg(destination, file!);
+    setState(() {});
+
+    if (task == null) return "Empty";
+
+    final snapshot = await task!.whenComplete(() {});
+    final urlDownload = await snapshot.ref.getDownloadURL();
+    print('Download-Link: $urlDownload');
+    return urlDownload;
+  }
+
+  uploadImg(String destination, File file) {
+    try {
+      final ref = FirebaseStorage.instance.ref(destination);
+
+      return ref.putFile(file);
+    } on FirebaseException catch (e) {
+      return null;
+    }
   }
 }
